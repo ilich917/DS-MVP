5fc274a034d0d48860efdf1bc25f6c8ec49653b4
ilich917 <64826916+ilich917@users.noreply.github.com>
2020-12-15 02:31:36 -0300

    add notebooks for keypoints extraction with openpose

--- yout_generate_pose.py
+++ yout_generate_pose.py
@@ -1,17 +1,26 @@
-"""
-Script para generar los archivos json desde un set de videos
-Editar los path
-"""
-
 import os
-dataset_path = 'Bible_j316_new'
+from moviepy.video.VideoClip import VideoClip
+from moviepy.video.io.VideoFileClip import VideoFileClip
+
+from moviepy.video.fx.resize import resize
+VideoFileClip.resize = resize
+
+#from moviepy.video.fx import resize
+dataset_path = 'PREPROCESSING/Videos'
+dataset_path2 = 'PREPROCESSING/Videos_raw'
 
 for sub in os.listdir(dataset_path):
     for file in os.listdir(dataset_path + '/' + sub):
-        for f in os.listdir(dataset_path+'/'+sub+'/'+file):
-            # print(f)
-            video = dataset_path + '/' + sub + '/' + file + '/' + f
-            #command = 'bin\OpenPoseDemo.exe --video {} --hand --write_json ./output/{}'.format(video, sub)
-            command = r'bin\OpenPoseDemo.exe' --video "{}" --model_ppose COCO --hand --write_json "./new_bible_out/{}{}"'.format(video, sub, file)
-            print("executing ...", command)
-            os.system(command)
+            video = dataset_path + '/' + sub + '/' + file
+            clip = VideoFileClip(video)
+            clip = clip.resize(0.3)
+            #os.remove(video)
+            video2 = dataset_path2 + '/' + sub + '/' + file
+            clip.write_videofile(video2)
+            clip.close()
+            #os.makedirs('PREPROCESSING/Pose/' + sub + '/' + file[:-4])
+            #command = r'C:\Users\Fernando\Documents\GitHub\Openpose-gpu\openpose\bin\OpenPoseDemo.exe --video {} --display 0 --model_pose BODY_25 --render_pose 0 --number_people_max 1 --model_folder C:\Users\Fernando\Documents\GitHub\Openpose-gpu\openpose\models --hand --face --write_json "PREPROCESSING/Pose/{}/{}"'.format(video, sub, file[:-4])
+            #print("executing ...", command)
+            #os.system(command)
+
+# TENGO QUE VOLVER A GENERAR LOS VIDEOS. PODRÍA HACER RESIZE AL ARCHIVO ORIGINAL...
